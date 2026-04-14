12926e36b6d78cac582afa381c50814f69ec876e
Motoyuki Kimura <moymoymox@gmail.com>
2026-04-14 10:45:00 +0900

    feat: add hello world

--- sobu_line_notifier/src/bin/main.rs
+++ sobu_line_notifier/src/bin/main.rs
@@ -9,10 +9,24 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_time::{Delay, Duration, Timer};
+use embedded_hal_bus::spi::ExclusiveDevice;
+use esp_hal::Async;
 use esp_hal::clock::CpuClock;
+use esp_hal::dma::{DmaRxBuf, DmaTxBuf};
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::spi::Mode;
+use esp_hal::spi::master::{Config as SpiConfig, Spi};
+use esp_hal::time::Rate;
 use esp_hal::timer::timg::TimerGroup;
 use esp_println as _;
+use mipidsi::{
+    Builder,
+    interface::SpiInterface,
+    models::ST7789,
+    options::{ColorInversion, ColorOrder},
+};
+use static_cell::StaticCell;
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -42,6 +56,68 @@ async fn main(spawner: Spawner) -> ! {
     esp_rtos::start(timg0.timer0);
 
     info!("Embassy initialized!");
+
+    // Setup DMA for SPI
+    let dma_channel = peripherals.DMA_SPI2;
+    let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = esp_hal::dma_buffers!(512);
+    let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+    let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+    // Setup SPI bus for display
+    let spi_bus = Spi::new(
+        peripherals.SPI2,
+        SpiConfig::default()
+            .with_frequency(Rate::from_mhz(2))
+            .with_mode(Mode::_0),
+    )
+    .unwrap()
+    .with_sck(peripherals.GPIO13)
+    .with_mosi(peripherals.GPIO15)
+    .with_dma(dma_channel)
+    .with_buffers(dma_rx_buf, dma_tx_buf)
+    .into_async();
+
+    // Setup display pins
+    let _back_light = Output::new(peripherals.GPIO27, Level::High, OutputConfig::default());
+    let dc = Output::new(peripherals.GPIO14, Level::Low, OutputConfig::default());
+    let cs = Output::new(peripherals.GPIO5, Level::High, OutputConfig::default());
+    let rst = Output::new(peripherals.GPIO12, Level::High, OutputConfig::default());
+
+    // Create SPI device
+    let spi_device: ExclusiveDevice<esp_hal::spi::master::SpiDmaBus<'_, Async>, Output<'_>, Delay> =
+        ExclusiveDevice::new(spi_bus, cs, Delay).unwrap();
+
+    // Create display interface
+    static SPI_BUF: StaticCell<[u8; 512]> = StaticCell::new();
+    let spi_buffer = SPI_BUF.init([0u8; 512]);
+    let di = SpiInterface::new(spi_device, dc, spi_buffer);
+
+    // Initialize display
+    let mut display = Builder::new(ST7789, di)
+        .reset_pin(rst)
+        .display_size(135, 240)
+        .display_offset(52, 40)
+        .color_order(ColorOrder::Rgb)
+        .invert_colors(ColorInversion::Inverted)
+        .init(&mut Delay)
+        .unwrap();
+
+    info!("Display initialized!");
+
+    // Clear display to confirm it's working (black screen)
+    use embedded_graphics_core::pixelcolor::{Rgb565, RgbColor};
+    use embedded_graphics::{
+        mono_font::{ascii::FONT_6X10, MonoTextStyle},
+        prelude::*,
+        text::Text,
+    };
+    display.clear(Rgb565::BLACK).ok();
+
+    // Display text
+    let text_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+    Text::new("Hello M5StickC!", Point::new(10, 30), text_style)
+        .draw(&mut display)
+        .ok();
 
     // TODO: Spawn some tasks
     let _ = spawner;

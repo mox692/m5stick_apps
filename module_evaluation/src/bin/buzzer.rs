bc23dedd773f3f4f238e7cd951110d0121f9b57f
Motoyuki Kimura <moymoymox@gmail.com>
2026-05-03 09:50:17 +0900

    module_eval: add buzzer example

---
+++ module_evaluation/src/bin/buzzer.rs
@@ -0,0 +1,52 @@
+#![no_std]
+#![no_main]
+#![deny(clippy::large_stack_frames)]
+
+use esp_backtrace as _;
+use esp_hal::{
+    gpio::{DriveMode, DriveStrength, Level, Output, OutputConfig},
+    main,
+};
+use log::info;
+
+extern crate alloc;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[main]
+fn main() -> ! {
+    // generator version: 1.2.0
+
+    // Use default CPU clock to avoid UART baud rate issues
+    let config = esp_hal::Config::default();
+    // Start the system
+    let peripherals = esp_hal::init(config);
+
+    esp_println::logger::init_logger_from_env();
+
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 1024);
+
+    info!("System initialized!");
+
+    // Configure GPIO Pins as input with pull-up resistor
+    let config = OutputConfig::default()
+        .with_drive_mode(DriveMode::PushPull)
+        .with_drive_strength(DriveStrength::_5mA);
+
+    let mut buzzer = Output::new(peripherals.GPIO2, Level::Low, config);
+
+    let delay = esp_hal::delay::Delay::new();
+    loop {
+        buzzer.toggle();
+        info!("Hello from M5Stack C!");
+        delay.delay_millis(100);
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
+}
